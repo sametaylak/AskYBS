fd58168b61fe0a2428e7635d1a2e5468a8465766
muratkuru <muratkuru1995@gmail.com>
2017-07-16 13:15:33 +0300

    Added "kımıl kımıl" effects.

--- components/LogIn.vue
+++ components/LogIn.vue
@@ -1,48 +1,50 @@
 <template lang="html">
-  <div class="modal" :class="{'is-active': show}">
-    <div class="modal-background" @click="$emit('close')"></div>
-    <div class="modal-content">
-      <notify @close="showNotify = false" v-if="showNotify" message="Lütfen girmiş olduğunuz bilgileri kontrol ediniz."/>
-      <div class="card">
-        <header class="card-header">
-          <p class="card-header-title">
-            Giriş Yap
-          </p>
-        </header>
-        <div class="card-content">
-          <div class="content">
-            <form @submit.prevent="login">
-              <div class="field">
-                <p class="control has-icons-left">
-                  <input v-model="email" class="input" type="email" placeholder="Email" required>
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-envelope"></i>
-                  </span>
-                </p>
-              </div>
-              <div class="field">
-                <p class="control has-icons-left">
-                  <input v-model="password" class="input" type="password" placeholder="Parola" required>
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-lock"></i>
-                  </span>
-                </p>
-              </div>
-              <div class="field">
-                <p class="control">
-                  <button class="button is-success">
-                    <i v-if="loading" class="loader"></i>
-                    Giriş
-                  </button>
-                </p>
-              </div>
-            </form>
+  <transition name="fade">
+    <div class="modal" :class="{'is-active': show}">
+      <div class="modal-background" @click="$emit('close')"></div>
+      <div class="modal-content">
+        <notify @close="showNotify = false" v-if="showNotify" message="Lütfen girmiş olduğunuz bilgileri kontrol ediniz."/>
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title">
+              Giriş Yap
+            </p>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              <form @submit.prevent="login">
+                <div class="field">
+                  <p class="control has-icons-left">
+                    <input v-model="email" class="input" type="email" placeholder="Email" required>
+                    <span class="icon is-small is-left">
+                      <i class="fa fa-envelope"></i>
+                    </span>
+                  </p>
+                </div>
+                <div class="field">
+                  <p class="control has-icons-left">
+                    <input v-model="password" class="input" type="password" placeholder="Parola" required>
+                    <span class="icon is-small is-left">
+                      <i class="fa fa-lock"></i>
+                    </span>
+                  </p>
+                </div>
+                <div class="field">
+                  <p class="control">
+                    <button class="button is-success">
+                      <i v-if="loading" class="loader"></i>
+                      Giriş
+                    </button>
+                  </p>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
+      <button class="modal-close is-large" @click="$emit('close')"></button>
     </div>
-    <button class="modal-close is-large" @click="$emit('close')"></button>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -51,7 +53,7 @@ import { mapActions } from 'vuex'
 import Notification from '~components/Notification'
 
 export default {
-  components: {'notify': Notification},
+  components: { 'notify': Notification },
   props: {
     show: {
       type: Boolean,
--- components/Register.vue
+++ components/Register.vue
@@ -1,56 +1,58 @@
 <template lang="html">
-  <div class="modal" :class="{'is-active': show}">
-    <div class="modal-background" @click="$emit('close')"></div>
-    <div class="modal-content">
-      <notify @close="showNotify = false" v-if="showNotify" message="Bir hata meydana geldi."/>
-      <div class="card">
-        <header class="card-header">
-          <p class="card-header-title">
-            Üye Ol
-          </p>
-        </header>
-        <div class="card-content">
-          <div class="content">
-            <form @submit.prevent="register">
-              <div class="field">
-                <p class="control has-icons-left">
-                  <input v-model="name" class="input" type="text" placeholder="Ad Soyad" required>
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-user"></i>
-                  </span>
-                </p>
-              </div>
-              <div class="field">
-                <p class="control has-icons-left">
-                  <input v-model="email" class="input" type="email" placeholder="Email" required>
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-envelope"></i>
-                  </span>
-                </p>
-              </div>
-              <div class="field">
-                <p class="control has-icons-left">
-                  <input v-model="password" class="input" type="password" placeholder="Parola" required>
-                  <span class="icon is-small is-left">
-                    <i class="fa fa-lock"></i>
-                  </span>
-                </p>
-              </div>
-              <div class="field">
-                <p class="control">
-                  <button class="button is-success">
-                    <i v-if="loading" class="loader"></i>
-                    Üye Ol
-                  </button>
-                </p>
-              </div>
-            </form>
+  <transition name="fade">
+    <div class="modal" :class="{'is-active': show}">
+      <div class="modal-background" @click="$emit('close')"></div>
+      <div class="modal-content">
+        <notify @close="showNotify = false" v-if="showNotify" message="Bir hata meydana geldi."/>
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title">
+              Üye Ol
+            </p>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              <form @submit.prevent="register">
+                <div class="field">
+                  <p class="control has-icons-left">
+                    <input v-model="name" class="input" type="text" placeholder="Ad Soyad" required>
+                    <span class="icon is-small is-left">
+                      <i class="fa fa-user"></i>
+                    </span>
+                  </p>
+                </div>
+                <div class="field">
+                  <p class="control has-icons-left">
+                    <input v-model="email" class="input" type="email" placeholder="Email" required>
+                    <span class="icon is-small is-left">
+                      <i class="fa fa-envelope"></i>
+                    </span>
+                  </p>
+                </div>
+                <div class="field">
+                  <p class="control has-icons-left">
+                    <input v-model="password" class="input" type="password" placeholder="Parola" required>
+                    <span class="icon is-small is-left">
+                      <i class="fa fa-lock"></i>
+                    </span>
+                  </p>
+                </div>
+                <div class="field">
+                  <p class="control">
+                    <button class="button is-success">
+                      <i v-if="loading" class="loader"></i>
+                      Üye Ol
+                    </button>
+                  </p>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
+      <button class="modal-close is-large" @click="$emit('close')"></button>
     </div>
-    <button class="modal-close is-large" @click="$emit('close')"></button>
-  </div>
+  </transition>
 </template>
 
 <script>
